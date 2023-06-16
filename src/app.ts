0e7fa77837ea54d9ce2a89bd7a3b19329ac95f2c
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2023-06-16 21:34:30 +0200

    added CORS

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,14 @@ import { errorMiddleware, logMiddleware } from "./utils/middleware";
 
 const app = express();
 app.use(express.json());
-// app.use(
-// 	cors({
-// 		origin: process.env.CORS_ORIGIN || "http://localhost:4000",
-// 		optionsSuccessStatus: 200,
-// 		maxAge: parseInt(process.env.CORS_MAX_AGE as string, 10) || 86400,
-// 		allowedHeaders: ["Accept", "Content-Type", "Authorization"],
-// 	})
-// );
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:4000",
+    optionsSuccessStatus: 200,
+    maxAge: parseInt(process.env.CORS_MAX_AGE as string, 10) || 86400,
+    allowedHeaders: ["Accept", "Content-Type", "Authorization"],
+  })
+);
 
 app.use(logMiddleware);
 installRestRouter(app);
